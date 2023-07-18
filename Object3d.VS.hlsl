0492e1571f7e047d84670ee294c4a83686c19a5d
LC1D_20_ナカムラタカアキ <k022g0031@g.neec.ac.jp>
2023-07-18 12:09:09 +0900

    例外スロー

---
+++ Object3d.VS.hlsl
@@ -0,0 +1,25 @@
+//float4 main( float4 pos : POSITION ) : SV_POSITION
+//{
+//	return pos;
+//}
+
+//struct TransformationMatrix {
+//	float32_t4x4 WVP;
+//};
+//ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
+
+
+struct VertexShaderOutput {
+	float32_t4 position : SV_POSITION;
+};
+
+struct VertexShaderInput {
+	float32_t4 position : POSITION0;
+};
+
+VertexShaderOutput main(VertexShaderInput input) {
+	VertexShaderOutput output;
+	output.position = input.position;
+	//output.position = mul(input.position, gTransformationMatrix.WVP);
+	return output;
+}
